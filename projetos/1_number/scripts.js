be27210c5d4a15a862c80f34ff87968d64e2c5a5
Elaine <elainerleandro1@gmail.com>
2022-07-12 13:22:39 -0300

    revisão conceitos js.

---
+++ projetos/1_number/scripts.js
@@ -0,0 +1,3 @@
+console.log( typeof 8)
+console.log(typeof 37.5)
+console.log( typeof -120)
---
+++ projetos/2_operacao_aritmetrica/scripts.js
@@ -0,0 +1,6 @@
+console.log( typeof (5 + 5));
+console.log(10 - 5);
+console.log(4 * 2);
+console.log(5 / 3);
+console.log(20 % 2);
+console.log(31 % 2);
